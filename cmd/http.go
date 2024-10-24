520e3de997409082c2539f0316e892ca2f703f2e
Dicky Pratama <dicky.pratama@arthasemesta.com>
2024-10-24 15:08:59 +0700

    add api register

--- cmd/http.go
+++ cmd/http.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"ewallet-ums/helpers"
 	"ewallet-ums/internal/api"
+	"ewallet-ums/internal/repository"
 	"ewallet-ums/internal/services"
 	"log"
 
@@ -15,9 +16,22 @@ func ServeHTTP() {
 		HealthcheckServices: healthcheckSvc,
 	}
 
+	registerRepo := &repository.RegisterRepository{
+		DB: helpers.DB,
+	}
+	registerSvc := &services.RegisterService{
+		RegisterRepo: registerRepo,
+	}
+	registerAPI := &api.RegisterHandler{
+		RegisterService: registerSvc,
+	}
+
 	r := gin.Default()
 
 	r.GET("/health", healthcheckAPI.HealthcheckHandlerHTTP)
+
+	userV1 := r.Group("/user/v1")
+	userV1.POST("/register", registerAPI.Register)
 
 	err := r.Run(":" + helpers.GetEnv("PORT", ""))
 	if err != nil {
