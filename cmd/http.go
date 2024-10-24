8ae050a8128e7140473fee99a8ae564fb0d97c55
Dicky Pratama <dicky.pratama@arthasemesta.com>
2024-10-25 01:33:36 +0700

    add api token validation using gRPC

--- cmd/http.go
+++ cmd/http.go
@@ -40,6 +40,8 @@ type Dependency struct {
 	LoginAPI        interfaces.ILoginHandler
 	LogoutAPI       interfaces.ILogoutHandler
 	RefreshTokenAPI interfaces.IRefreshTokenHandler
+
+	TokenValidationAPI *api.TokenValidationHandler
 }
 
 func dependencyInject() Dependency {
@@ -80,12 +82,20 @@ func dependencyInject() Dependency {
 		RefreshTokenService: refreshTokenSvc,
 	}
 
+	tokenValidationSvc := &services.TokenValidationService{
+		UserRepo: userRepo,
+	}
+	tokenValidationAPI := &api.TokenValidationHandler{
+		TokenValidationService: tokenValidationSvc,
+	}
+
 	return Dependency{
-		UserRepository:  userRepo,
-		HealthcheckAPI:  healthcheckAPI,
-		RegisterAPI:     registerAPI,
-		LoginAPI:        loginAPI,
-		LogoutAPI:       logoutAPI,
-		RefreshTokenAPI: refreshTokenAPI,
+		UserRepository:     userRepo,
+		HealthcheckAPI:     healthcheckAPI,
+		RegisterAPI:        registerAPI,
+		LoginAPI:           loginAPI,
+		LogoutAPI:          logoutAPI,
+		RefreshTokenAPI:    refreshTokenAPI,
+		TokenValidationAPI: tokenValidationAPI,
 	}
 }
